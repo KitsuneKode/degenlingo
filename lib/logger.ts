8b81195c4465d0efcf01a03d2b09393b97522c4b
Manash Pratim Bhuyan <bhuyanmanash2002@gmail.com>
2025-06-02 04:22:36 +0530

    remove logs to file

--- lib/logger.ts
+++ lib/logger.ts
@@ -13,17 +13,17 @@ const logger = createLogger({
     }),
   ),
   defaultMeta: { service: 'degenlingo' },
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `quick-start-combined.log`.
-    // - Write all logs error (and below) to `quick-start-error.log`.
-    //
-    new transports.File({
-      filename: '@/logs/error.log',
-      level: 'error',
-    }),
-    new transports.File({ filename: '@/logs/combined.log' }),
-  ],
+  // transports: [
+  //   //
+  //   // - Write to all logs with level `info` and below to `quick-start-combined.log`.
+  //   // - Write all logs error (and below) to `quick-start-error.log`.
+  //   //
+  //   new transports.File({
+  //     filename: '@/logs/error.log',
+  //     level: 'error',
+  //   }),
+  //   new transports.File({ filename: '@/logs/combined.log' }),
+  // ],
 })
 
 //
